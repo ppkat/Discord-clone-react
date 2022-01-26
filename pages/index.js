348159cd8ea61781f0ed0824edf30caf571a9add
Joao Pedro <joaopedro@sobremail.com>
2022-01-26 18:37:35 -0300

    provando react

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,45 @@
+function GlobalStyle(args) {
+
+    return(
+        <style global jsx>{`
+            *{
+                margin: 0;
+                padding: 0;
+                border-sizing: border-box;
+            }
+        `}</style>
+    )
+    
+}
+
+function PageTitle(tag) {
+    const Type = tag.type || 'h1'
+    return(
+        <>
+            <Type>{tag.children}</Type>
+
+            <style jsx>{`
+        
+                ${Type}{
+                    color: blue;
+                    text-align: center;
+                    font-size: 80px;
+                    font-weight: 1300px
+                }
+
+            `}</style>
+        </>
+        
+    )
+}
+
+
 //componente react
 function HomePage() {
     return(
         <div>
-            <h1>Minha primeira página react</h1>
+            <PageTitle type="h2">Minha primeira página com react</PageTitle>
+            <p>Bem vindo a minha primeira página react</p>
         </div>
     )
   }
